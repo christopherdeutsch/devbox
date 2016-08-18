9da4a1a6d447c6d2338f1719a8a2a34bb34d3bd8
Chris Deutsch <chef@ispeakdeutsch.com>
2016-08-18 00:51:06 -0700

    fix rbenv build recipe order

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -1,7 +1,7 @@
 #
 # Use the rbenv cookbook to set up ruby 
 #
-include_recipe 'ruby_rbenv::system'
 include_recipe 'ruby_build::default'
+include_recipe 'ruby_rbenv::system'
 
 rbenv_global node['rbenv']['rubies'].first

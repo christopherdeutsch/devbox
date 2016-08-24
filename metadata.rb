1d79b564cc77f5afd75c0f604df7ac380910a47c
Chris <chef@ispeakdeutsch.com>
2016-08-24 11:06:18 -0700

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chef@ispeakdeutsch.com'
 license          'BSD'
 description      'Installs/Configures a devbox'
 long_description 'Installs/Configures a devbox'
-version          '0.2.0'
+version          '0.3.0'
 
 depends 'ruby_rbenv'
 depends 'ruby_build'

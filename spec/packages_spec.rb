a048a8d7ab1f39236a41468611347193394db12c
Chris <chef@ispeakdeutsch.com>
2016-08-24 16:56:53 -0700

    add package tests

--- spec/packages_spec.rb
+++ spec/packages_spec.rb
@@ -1,9 +1,22 @@
 describe 'devbox::packages' do
-  let(:chef_run) { ChefSpec::ServerRunner.new.converge(described_recipe) }
-  
-  %w(joe build-essential rbenv ruby-build ssl-cert).each do |pkg|
-    it "creates directory #{pkg}" do
-      expect(chef_run).to install_package(pkg)
-    end
+  let(:chef_run) do
+    ChefSpec::ServerRunner.new do |node|
+      node.automatic['platform'] = 'platform'
+      node.normal['devbox']['packages']['platform']['editor']     = %w(editor_pkg)
+      node.normal['devbox']['packages']['platform']['build']      = %w(build_pkg)
+      node.normal['devbox']['packages']['platform']['misc']       = %w(misc_pkg)
+    end.converge(described_recipe)
+  end
+
+  it 'installs editor packages' do
+    expect(chef_run).to install_package('editor_pkg')
+  end
+
+  it 'installs build packages' do
+    expect(chef_run).to install_package('build_pkg')
+  end
+
+  it 'installs misc packages' do
+    expect(chef_run).to install_package('misc_pkg')
   end
 end

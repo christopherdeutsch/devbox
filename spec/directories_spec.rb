b54b429e6a86505ca721c3cb991ad246a9ab2ada
Chris <chef@ispeakdeutsch.com>
2016-08-24 17:21:10 -0700

    add directory tests

--- spec/directories_spec.rb
+++ spec/directories_spec.rb
@@ -1,9 +1,22 @@
 describe 'devbox::directories' do
-  let(:chef_run) { ChefSpec::ServerRunner.new.converge(described_recipe) }
-  
-  %w(/home/cdeutsch /home/cdeutsch/src /home/cdeutsch/packages).each do |dir|
-    it "creates directory #{dir}" do
-      expect(chef_run).to create_directory(dir)
+  [
+    ['unix', 'home'],
+    ['mac_os_x', 'Users']
+  ].each do |platform, dir_prefix|
+    context platform do
+      let(:chef_run) do
+        ChefSpec::ServerRunner.new do |node|
+          node.automatic['platform'] = platform
+          node.normal['devbox']['user'] = 'user'
+        end.converge(described_recipe)
+      end
+
+      %w(src packages).each do |dir|
+        it "creates directory #{dir}" do
+          expect(chef_run).to create_directory("/#{dir_prefix}/user/#{dir}")
+        end
+      end
     end
   end
 end
+
